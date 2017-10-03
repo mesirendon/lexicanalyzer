5150e70607011c75acae8d80f37f337b76e76031
Mesi Rendon <amrendonsa@unal.edu.co>
2017-10-02 20:04:38 -0500

    Fix on presentation

--- analyzer_simple.py
+++ analyzer_simple.py
@@ -1,138 +1,154 @@
 import sys
 import ply.lex as lex
 
+class Lexer:
+    data = ''
 # Palabras reservadas
-reserved = {
-    'set'           : 'set',
-    'gets'          : 'gets',
-    'puts'          : 'puts',
-    'expr'          : 'expr',
-    'if'            : 'if',
-    'then'          : 'then',
-    'elseif'        : 'elseif',
-    'else'          : 'else',
-    'switch'        : 'switch',
-    'default'       : 'default',
-    'while'         : 'while',
-    'continue'      : 'continue',
-    'break'         : 'break',
-    'incr'          : 'incr',
-    'for'           : 'for',
-#    'arraysize'    : 'arraysize',
-#    'array exists'  : 'arrayexists',
-    'array'         : 'array',
-    'proc'          : 'proc',
-    'return'        : 'return',
-}
+    reserved = {
+        'set'      : 'set',
+        'gets'     : 'gets',
+        'puts'     : 'puts',
+        'expr'     : 'expr',
+        'if'       : 'if',
+        'then'     : 'then',
+        'elseif'   : 'elseif',
+        'else'     : 'else',
+        'switch'   : 'switch',
+        'default'  : 'default',
+        'while'    : 'while',
+        'continue' : 'continue',
+        'break'    : 'break',
+        'incr'     : 'incr',
+        'for'      : 'for',
+        'size'     : 'size',
+        'exists'   : 'exists',
+        'array'    : 'array',
+        'proc'     : 'proc',
+        'return'   : 'return',
+    }
 
-tokens = [
-        'token_integer',
-        'token_double',
-        'token_string',
-        'token_llave_izq',
-        'token_llave_der',
-        'token_dollar',
-        'token_pyc',
-        'token_cor_izq',
-        'token_cor_der',
-        'token_par_izq',
-        'token_par_der',
-        'token_mayor',
-        'token_menor',
-        'token_mayor_igual',
-        'token_menor_igual',
-        'token_igual_str',
-        'token_igual_num',
-        'token_diff_str',
-        'token_diff_num',
-        'token_and',
-        'token_or',
-        'token_not',
-        'token_mas',
-        'token_menos',
-        'token_mul',
-        'token_div',
-        'token_mod',
-        'token_pot',
-        ] + list(reserved.values())
+    tokens = [
+            'integer',
+            'double',
+            'string',
+            'token_llave_izq',
+            'token_llave_der',
+            'token_dollar',
+            'token_pyc',
+            'token_cor_izq',
+            'token_cor_der',
+            'token_par_izq',
+            'token_par_der',
+            'token_mayor',
+            'token_menor',
+            'token_mayor_igual',
+            'token_menor_igual',
+            'token_igual_str',
+            'token_igual_num',
+            'token_diff_str',
+            'token_diff_num',
+            'token_and',
+            'token_or',
+            'token_not',
+            'token_mas',
+            'token_menos',
+            'token_mul',
+            'token_div',
+            'token_mod',
+            'token_pot',
+            ] + list(reserved.values())
 
 # Tokens simples
-t_token_llave_izq   = r'\{'
-t_token_llave_der   = r'\}'
-t_token_dollar      = r'\$'
-t_token_pyc         = r'\;'
-t_token_cor_izq     = r'\['
-t_token_cor_der     = r'\]'
-t_token_par_izq     = r'\('
-t_token_par_der     = r'\)'
-t_token_mayor       = r'\>'
-t_token_menor       = r'\<'
-t_token_mayor_igual = r'\>='
-t_token_menor_igual = r'\<='
-t_token_igual_str   = r'eq'
-t_token_igual_num   = r'\=='
-t_token_diff_str    = r'ne'
-t_token_diff_num    = r'\!='
-t_token_and         = r'\&&'
-t_token_or          = r'\|\|'
-t_token_pot         = r'\*\*'
-t_token_not         = r'\!'
-t_token_mas         = r'\+'
-t_token_menos       = r'\-'
-t_token_mul         = r'\*'
-t_token_div         = r'\/'
-t_token_mod         = r'\%'
+    t_token_llave_izq   = r'\{'
+    t_token_llave_der   = r'\}'
+    t_token_dollar      = r'\$'
+    t_token_pyc         = r'\;'
+    t_token_cor_izq     = r'\['
+    t_token_cor_der     = r'\]'
+    t_token_par_izq     = r'\('
+    t_token_par_der     = r'\)'
+    t_token_mayor       = r'>'
+    t_token_menor       = r'<'
+    t_token_mayor_igual = r'>='
+    t_token_menor_igual = r'<='
+    t_token_igual_str   = r'eq'
+    t_token_igual_num   = r'=='
+    t_token_diff_str    = r'ne'
+    t_token_diff_num    = r'!='
+    t_token_and         = r'\&&'
+    t_token_or          = r'\|\|'
+    t_token_not         = r'!'
+    t_token_mas         = r'\+'
+    t_token_menos       = r'-'
+    t_token_mul         = r'\*'
+    t_token_div         = r'\/'
+    t_token_mod         = r'%'
+    t_token_pot         = r'\*\*'
 
 
+    def t_double(self, t):
+        r'\d+\.\d+'
+        t.value = float(t.value)
+        return t
 
-def t_token_double(t):
-    r'\d+\.\d+'
-    t.value = float(t.value)
-    return t
+    def t_integer(self, t):
+        r'\d+'
+        t.value = int(t.value)
+        return t
 
-def t_token_integer(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
+    def t_string(self, t):
+        r'\".+\"'
+        t.value = str(t.value)
+        return t
 
-def t_token_string(t):
-    r'\".+\"'
-    t.value = str(t.value)
-    return t
+    def t_reserved(self, t):
+        r'[a-zA-Z_][a-zA-Z_0-9]*'
+        t.type = self.reserved.get(t.value,'ID')
+        return t
 
-def t_reserved(t):
-    r'[a-z_][a-z_]*'
-    t.type = reserved.get(str(t.value))
-    return t
+    def t_newline(self, t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)
 
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
+    def t_error(self, t):
+        print("Illegal character '%s'" % t.value[0])
+        t.lexer.skip(1)
 
-def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
-    t.lexer.skip(1)
+    t_ignore = ' \t'
 
-t_ignore = ' \t'
+    def find_column(self, token):
+        last_cr = self.data.rfind('\n', 0, token.lexpos)
+        if last_cr < 0:
+            last_cr = 0
+            column = (token.lexpos - last_cr) + 1
+        else:
+            column = (token.lexpos - last_cr)
+        return column
 
-lex.lex()
+    def build(self, data):
+        self.lexer = lex.lex(module=self)
+        self.data = data
+
+    def lexerRun(self):
+        self.lexer.input(self.data)
+        while True:
+            tok = self.lexer.token()
+            if not tok:
+                break
+            if (tok.type in ["id", "integer", "string", "double"]):
+                print("<", tok.type, ",", tok.value, ",", str(tok.lineno), ",", str(self.find_column(tok)), ">")
+            else:
+                print("<", tok.type + ",", str(tok.lineno), ",", str(self.find_column(tok)), ">")
+
+
 
 data = ''
+
 for line in sys.stdin:
     if line.strip().startswith("#"):
         continue
     data += line
 
-
-lex.input(data)
-
-# Tokenize
-cr = 0
-while True:
-    tok = lex.token()
-    if not tok:
-        break      # No more input
-    if str(tok.type) == 'token_string' or str(tok.type) == 'token_integer' or str(tok.type) == 'token_double':
-        print("<" + tok.type + "," + str(tok.value) + "," + str(tok.lineno) + "," + str(tok.lexpos) + ">")
-    else:
-        print("<" + tok.type + "," + str(tok.lineno) + "," + str(tok.lexpos) + ">")
+m = Lexer()
+m.build(data)
+m.lexerRun()
